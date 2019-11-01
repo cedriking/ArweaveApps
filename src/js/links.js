52d6f190154da39a81033c98eea5ef4a0edb10c4
Cedrik <cedrik@arweave.org>
2019-11-01 15:56:17 -0700

    votes and cache updated

--- src/js/links.js
+++ src/js/links.js
@@ -77,92 +77,92 @@ class Links {
                     }
 
                     str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
-                        .replace(/\s+/g, '-') // collapse whitespace and replace by -
-                        .replace(/-+/g, '-'); // collapse dashes
+                      .replace(/\s+/g, '-') // collapse whitespace and replace by -
+                      .replace(/-+/g, '-'); // collapse dashes
 
                     return str;
                 }
 
                 const gradients = [
-                  'background: #9D7AF7;\n' +
-                  'background: -webkit-linear-gradient(right, #9D7AF7, #1192B1);\n' +
-                  'background: -moz-linear-gradient(right, #9D7AF7, #1192B1);\n' +
-                  'background: linear-gradient(to left, #9D7AF7, #1192B1);',
+                    'background: #9D7AF7;\n' +
+                    'background: -webkit-linear-gradient(right, #9D7AF7, #1192B1);\n' +
+                    'background: -moz-linear-gradient(right, #9D7AF7, #1192B1);\n' +
+                    'background: linear-gradient(to left, #9D7AF7, #1192B1);',
 
-                  'background: #E04A82;\n' +
-                  'background: -webkit-linear-gradient(right, #E04A82, #5A2B57);\n' +
-                  'background: -moz-linear-gradient(right, #E04A82, #5A2B57);\n' +
-                  'background: linear-gradient(to left, #E04A82, #5A2B57);',
+                    'background: #E04A82;\n' +
+                    'background: -webkit-linear-gradient(right, #E04A82, #5A2B57);\n' +
+                    'background: -moz-linear-gradient(right, #E04A82, #5A2B57);\n' +
+                    'background: linear-gradient(to left, #E04A82, #5A2B57);',
 
-                  'background: #005179;\n' +
-                  'background: -webkit-linear-gradient(right, #005179, #7A5CBA);\n' +
-                  'background: -moz-linear-gradient(right, #005179, #7A5CBA);\n' +
-                  'background: linear-gradient(to left, #005179, #7A5CBA);',
+                    'background: #005179;\n' +
+                    'background: -webkit-linear-gradient(right, #005179, #7A5CBA);\n' +
+                    'background: -moz-linear-gradient(right, #005179, #7A5CBA);\n' +
+                    'background: linear-gradient(to left, #005179, #7A5CBA);',
 
-                  'background: #95D5F1;\n' +
-                  'background: -webkit-linear-gradient(right, #95D5F1, #255A4A);\n' +
-                  'background: -moz-linear-gradient(right, #95D5F1, #255A4A);\n' +
-                  'background: linear-gradient(to left, #95D5F1, #255A4A);',
+                    'background: #95D5F1;\n' +
+                    'background: -webkit-linear-gradient(right, #95D5F1, #255A4A);\n' +
+                    'background: -moz-linear-gradient(right, #95D5F1, #255A4A);\n' +
+                    'background: linear-gradient(to left, #95D5F1, #255A4A);',
 
-                  'background: #E37A5E;\n' +
-                  'background: -webkit-linear-gradient(right, #E37A5E, #8907D6);\n' +
-                  'background: -moz-linear-gradient(right, #E37A5E, #8907D6);\n' +
-                  'background: linear-gradient(to left, #E37A5E, #8907D6);',
+                    'background: #E37A5E;\n' +
+                    'background: -webkit-linear-gradient(right, #E37A5E, #8907D6);\n' +
+                    'background: -moz-linear-gradient(right, #E37A5E, #8907D6);\n' +
+                    'background: linear-gradient(to left, #E37A5E, #8907D6);',
 
-                  'background: #93F74A;\n' +
-                  'background: -webkit-linear-gradient(right, #93F74A, #327093);\n' +
-                  'background: -moz-linear-gradient(right, #93F74A, #327093);\n' +
-                  'background: linear-gradient(to left, #93F74A, #327093);',
+                    'background: #93F74A;\n' +
+                    'background: -webkit-linear-gradient(right, #93F74A, #327093);\n' +
+                    'background: -moz-linear-gradient(right, #93F74A, #327093);\n' +
+                    'background: linear-gradient(to left, #93F74A, #327093);',
 
-                  'background: #6E3AF9;\n' +
-                  'background: -webkit-linear-gradient(right, #6E3AF9, #140E1E);\n' +
-                  'background: -moz-linear-gradient(right, #6E3AF9, #140E1E);\n' +
-                  'background: linear-gradient(to left, #6E3AF9, #140E1E);',
+                    'background: #6E3AF9;\n' +
+                    'background: -webkit-linear-gradient(right, #6E3AF9, #140E1E);\n' +
+                    'background: -moz-linear-gradient(right, #6E3AF9, #140E1E);\n' +
+                    'background: linear-gradient(to left, #6E3AF9, #140E1E);',
 
-                  'background: #E7C8C3;\n' +
-                  'background: -webkit-linear-gradient(right, #E7C8C3, #645427);\n' +
-                  'background: -moz-linear-gradient(right, #E7C8C3, #645427);\n' +
-                  'background: linear-gradient(to left, #E7C8C3, #645427);',
+                    'background: #E7C8C3;\n' +
+                    'background: -webkit-linear-gradient(right, #E7C8C3, #645427);\n' +
+                    'background: -moz-linear-gradient(right, #E7C8C3, #645427);\n' +
+                    'background: linear-gradient(to left, #E7C8C3, #645427);',
 
-                  'background: #BEB5C0;\n' +
-                  'background: -webkit-linear-gradient(right, #BEB5C0, #73585F);\n' +
-                  'background: -moz-linear-gradient(right, #BEB5C0, #73585F);\n' +
-                  'background: linear-gradient(to left, #BEB5C0, #73585F);',
+                    'background: #BEB5C0;\n' +
+                    'background: -webkit-linear-gradient(right, #BEB5C0, #73585F);\n' +
+                    'background: -moz-linear-gradient(right, #BEB5C0, #73585F);\n' +
+                    'background: linear-gradient(to left, #BEB5C0, #73585F);',
 
-                  'background: #0F7559;\n' +
-                  'background: -webkit-linear-gradient(right, #0F7559, #949862);\n' +
-                  'background: -moz-linear-gradient(right, #0F7559, #949862);\n' +
-                  'background: linear-gradient(to left, #0F7559, #949862);',
+                    'background: #0F7559;\n' +
+                    'background: -webkit-linear-gradient(right, #0F7559, #949862);\n' +
+                    'background: -moz-linear-gradient(right, #0F7559, #949862);\n' +
+                    'background: linear-gradient(to left, #0F7559, #949862);',
 
-                  'background: #4D0F98;\n' +
-                  'background: -webkit-linear-gradient(right, #4D0F98, #119467);\n' +
-                  'background: -moz-linear-gradient(right, #4D0F98, #119467);\n' +
-                  'background: linear-gradient(to left, #4D0F98, #119467);',
+                    'background: #4D0F98;\n' +
+                    'background: -webkit-linear-gradient(right, #4D0F98, #119467);\n' +
+                    'background: -moz-linear-gradient(right, #4D0F98, #119467);\n' +
+                    'background: linear-gradient(to left, #4D0F98, #119467);',
 
-                  'background: #293171;\n' +
-                  'background: -webkit-linear-gradient(right, #293171, #647B82);\n' +
-                  'background: -moz-linear-gradient(right, #293171, #647B82);\n' +
-                  'background: linear-gradient(to left, #293171, #647B82);',
+                    'background: #293171;\n' +
+                    'background: -webkit-linear-gradient(right, #293171, #647B82);\n' +
+                    'background: -moz-linear-gradient(right, #293171, #647B82);\n' +
+                    'background: linear-gradient(to left, #293171, #647B82);',
 
-                  'background: #F8D353;\n' +
-                  'background: -webkit-linear-gradient(right, #F8D353, #FF5C5A);\n' +
-                  'background: -moz-linear-gradient(right, #F8D353, #FF5C5A);\n' +
-                  'background: linear-gradient(to left, #F8D353, #FF5C5A);',
+                    'background: #F8D353;\n' +
+                    'background: -webkit-linear-gradient(right, #F8D353, #FF5C5A);\n' +
+                    'background: -moz-linear-gradient(right, #F8D353, #FF5C5A);\n' +
+                    'background: linear-gradient(to left, #F8D353, #FF5C5A);',
 
-                  'background: #D74F80;\n' +
-                  'background: -webkit-linear-gradient(right, #D74F80, #9241B5);\n' +
-                  'background: -moz-linear-gradient(right, #D74F80, #9241B5);\n' +
-                  'background: linear-gradient(to left, #D74F80, #9241B5);',
+                    'background: #D74F80;\n' +
+                    'background: -webkit-linear-gradient(right, #D74F80, #9241B5);\n' +
+                    'background: -moz-linear-gradient(right, #D74F80, #9241B5);\n' +
+                    'background: linear-gradient(to left, #D74F80, #9241B5);',
 
-                  'background: #CBC988;\n' +
-                  'background: -webkit-linear-gradient(right, #CBC988, #3F7890);\n' +
-                  'background: -moz-linear-gradient(right, #CBC988, #3F7890);\n' +
-                  'background: linear-gradient(to left, #CBC988, #3F7890);',
+                    'background: #CBC988;\n' +
+                    'background: -webkit-linear-gradient(right, #CBC988, #3F7890);\n' +
+                    'background: -moz-linear-gradient(right, #CBC988, #3F7890);\n' +
+                    'background: linear-gradient(to left, #CBC988, #3F7890);',
 
-                  'background: #B35D34;\n' +
-                  'background: -webkit-linear-gradient(right, #B35D34, #958B2A);\n' +
-                  'background: -moz-linear-gradient(right, #B35D34, #958B2A);\n' +
-                  'background: linear-gradient(to left, #B35D34, #958B2A);'
+                    'background: #B35D34;\n' +
+                    'background: -webkit-linear-gradient(right, #B35D34, #958B2A);\n' +
+                    'background: -moz-linear-gradient(right, #B35D34, #958B2A);\n' +
+                    'background: linear-gradient(to left, #B35D34, #958B2A);'
                 ];
 
                 let html = '';
@@ -225,8 +225,8 @@ class Links {
         }
 
         str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
-            .replace(/\s+/g, '-') // collapse whitespace and replace by -
-            .replace(/-+/g, '-'); // collapse dashes
+          .replace(/\s+/g, '-') // collapse whitespace and replace by -
+          .replace(/-+/g, '-'); // collapse dashes
 
         return str;
     }
@@ -254,6 +254,12 @@ class Links {
         for(let i = 0, j = transactions.length; i < j; i++) {
             const id = transactions[i].id;
 
+            let storedData = window.localStorage.getItem(`${app.appVersion}-${id}`);
+            if(storedData) {
+                this._data.push(JSON.parse(storedData));
+                continue;
+            }
+
             let txRow = {};
             const tx = await arweave.transactions.get(id);
 
@@ -274,15 +280,20 @@ class Links {
             });
 
             const tmpVotes = new Set();
-            for(let i = 0, j = transactions[i].votes.length; i < j; i++) {
-                tmpVotes.add(transactions[i].votes[i]);
+            for(let k = 0, l = transactions[i].votes.length; k < l; k++) {
+                tmpVotes.add(transactions[i].votes[k].id);
             }
             txRow['votes'] = Array.from(tmpVotes);
 
             this._data.push(txRow);
-            window.localStorage.setItem(id, JSON.stringify(txRow));
+
+            try {
+                window.localStorage.setItem(`${app.appVersion}-${id}`, JSON.stringify(txRow));
+            } catch (e) {}
+
         }
         console.timeEnd('grabbing app details');
+        console.log(this._data);
 
         this._dataById = new Map();
         console.time('filtering apps');
@@ -363,28 +374,6 @@ class Links {
         const linksId = await this.getAllLinksByAccount(address);
 
         let options = [];
-        /*if(linksId && linksId.length) {
-            options = await Promise.all(linksId.map(async linkId => {
-                let txRow = {};
-                const tx = await arweave.transactions.get(linkId);
-
-                tx.get('tags').forEach(tag => {
-                    let key = tag.get('name', { decode: true, string: true });
-                    let value = tag.get('value', { decode: true, string: true });
-                    txRow[key.toLowerCase()] = value
-                });
-
-                const data = tx.get('data', {decode: true, string: true});
-
-                txRow['id'] = linkId;
-                txRow['data'] = data;
-                txRow['from'] = await arweave.wallets.ownerToAddress(tx.owner);
-
-                return txRow;
-            }));
-        }
-
-        const optionsHtml = await this._workers.getUserPermawebs(options);*/
 
         for(let i = 0, j = linksId.length; i < j; i++) {
             options.push(`<option>${linksId[i]}</option>`);
